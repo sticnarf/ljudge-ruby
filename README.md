e38366c110996c71ae0048c593020b6c04cf2ce7
Yilin Chen <wormful@gmail.com>
2015-01-11 17:40:50 +0800

    implemented basic function

--- README.md
+++ README.md
@@ -22,4 +22,24 @@ Or install it yourself as:
 
 ## Usage
 
-TODO
+```ruby
+require 'ljudge'
+
+args = {
+  user_code: '/tmp/a.c',
+  checker_code: '/tmp/checker.rb',
+  testcase: [
+    { input: '/tmp/1.in', output: '/tmp/1.out' },
+    { input: '/tmp/2.in', output: '/tmp/2.out' }
+  ]
+}
+
+options = {
+  max_cpu_time: 1,
+  max_memory: 2**27,
+  max_compiler_cpu_time: 10
+}
+
+Ljudge.run(args, options)
+#=> {"checkerCompilation"=>{"log"=>"", "success"=>true}, "compilation"=>{"log"=>"", "success"=>true}, "testcases"=>[{"memory"=>131072, "result"=>"ACCEPTED", "time"=>0.001}, {"memory"=>221184, "result"=>"WRONG_ANSWER", "time"=>0.001}]}
+```
--- lib/ljudge.rb
+++ lib/ljudge.rb
@@ -1,5 +1,50 @@
 require "ljudge/version"
+require 'json'
 
 module Ljudge
-  # Your code goes here...
+
+  # Examples:
+  # args = {
+  #   user_code: '/tmp/code.c',
+  #   checker_code: '/tmp/checker.rb',
+  #   testcase: [
+  #     { input: '/tmp/1.in', output: '/tmp/1.out' },
+  #     { input: '/tmp/2.in', output: '/tmp/2.out' }
+  #   ]
+  # }
+
+  # options = {
+  #   max_cpu_time: 1,
+  #   max_memory: 2**27,
+  #   max_compiler_cpu_time: 10
+  # }
+
+  # env = {
+  #   name1: 'value1',
+  #   name2: 'value2'
+  # }
+  def self.run(args, options, env={})
+    command = "ljudge "
+    command << to_option_str(args) << to_option_str(options)
+    env.each { |key, value| command << "--env #{key} #{value} "}
+    output = IO.popen(command, err: [:child, :out]).readlines.join
+    begin
+      JSON::restore(output)
+    rescue JSON::ParserError
+      output
+    end
+  end
+
+  def self.to_option_str(hash)
+    str = ""
+    hash.each do |key, value|
+      if value.is_a? Array
+        value.each { |args| str << '--' << key.to_s.gsub(/_/, '-') << ' ' << to_option_str(args) }
+      else
+        str << '--' << key.to_s.gsub(/_/, '-') << ' '
+        str << value.to_s << ' '
+      end
+    end
+    str
+  end
 end
